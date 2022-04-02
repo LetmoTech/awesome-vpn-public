84b3e4940244d01c01ca75e3bd414d4b8af7d831
snitron <nvar3@yandex.ru>
2022-04-02 16:13:12 +0300

    Added raw bitcoin API

--- src/main/kotlin/org/kamikadzy/awesomevpn/db/user/UserService.kt
+++ src/main/kotlin/org/kamikadzy/awesomevpn/db/user/UserService.kt
@@ -1,15 +1,32 @@
 package org.kamikadzy.awesomevpn.db.user
 
+import org.kamikadzy.awesomevpn.utils.Constants
 import org.kamikadzy.awesomevpn.utils.unwrap
 import org.springframework.stereotype.Service
+import java.math.BigDecimal
 
 @Service
 class UserService(
-    val userRepository: UserRepository
+    val userRepository: UserRepository,
+    val constants: Constants
 ) {
     fun saveUser(user: User) = userRepository.save(user)
 
     fun removeUser(user: User) = userRepository.delete(user)
+
+    fun createUser(tgId: Long, chatId: Long, name: String?): User {
+        val newUser = User(
+            tgId = tgId,
+            chatId = chatId,
+            name = name
+        )
+
+        for ((cur, gateway) in constants.cryptoGateways) {
+            newUser.cryptoWallets[cur] = gateway.getNewAddress()
+        }
+
+        return userRepository.save(newUser)
+    }
 
     fun getAllUsers(): ArrayList<User> {
         return userRepository.findAll() as ArrayList<User>
@@ -40,5 +57,10 @@ class UserService(
         if (user != null) {
             userRepository.save(user)
         }
+    }
+
+
+    fun gotPayment(amount: BigDecimal) {
+
     }
 }
