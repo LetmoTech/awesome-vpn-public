4e4e7d5bd4833b08e40c36f95c3657b9423e7339
Nikita Varlamov <nvar3@yandex.ru>
2022-04-14 16:30:51 +0000

    Fixed job

--- .space.kts
+++ .space.kts
@@ -4,22 +4,19 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-job("Build") {
+job("Copy main repository") {
     git {
         depth = UNLIMITED_DEPTH
     }
-    
-    container(displayName = "Build environment", image = "openjdk:17") {
-        shellScript {
-            interpreter = "/bin/bash"
-            content = """
-                    cd /mnt/space/work/awesome-vpn
-                    microdnf install git
-                    git submodule init
-                    git submodule update --init --recursive
-                    ./gradlew
-                    gradle :build
-                """
-        }
+}
+
+job("Copy RisexAPI repository") {
+    git("RisexAPI") {
+        cloneDir = "awesome-vpn/RisexAPI"
+        depth = UNLIMITED_DEPTH
     }
+}
+
+job("Build and run tests") {
+   gradlew("openjdk:17", "build")
 }
