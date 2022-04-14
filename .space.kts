c50f21a450e106496c4d2a612bdef301e4852ace
Nikita Varlamov <nvar3@yandex.ru>
2022-04-14 16:35:31 +0000

    Fixed job

--- .space.kts
+++ .space.kts
@@ -4,19 +4,14 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-job("Copy main repository") {
-    git {
-        depth = UNLIMITED_DEPTH
-    }
-}
-
-job("Copy RisexAPI repository") {
-    git("RisexAPI") {
-        cloneDir = "awesome-vpn/RisexAPI"
-        depth = UNLIMITED_DEPTH
-    }
-}
-
 job("Build and run tests") {
+   git {
+       depth = UNLIMITED_DEPTH
+   }
+    
+   git("RisexAPI") {
+       cloneDir = "awesome-vpn/RisexAPI"
+       depth = UNLIMITED_DEPTH
+   }
    gradlew("openjdk:17", "build")
 }
